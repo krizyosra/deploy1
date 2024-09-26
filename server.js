655af3155389b23f05f6992bdd03eddb7fce9fce
krizyosra <krizyosra50@gmail.com>
2024-09-26 20:37:35 +0200

    cors2

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const cors = require("cors");
 const corsOptions = {
     origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
+   // allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true, 
   };
 
